a605c0ea6b4437fff4689cd6d4dc00c66616c776
Khrapov Denys <khrapov.denys@gmail.com>
2024-12-20 19:24:45 +0200

    added notification, header, two-block

--- js/main.js
+++ js/main.js
@@ -1,63 +1,79 @@
-document.addEventListener('DOMContentLoaded', function () {
-	updateThemePreference();
-	setupThemeToggle();
+document.addEventListener("DOMContentLoaded", function () {
+  updateThemePreference();
+  setupThemeToggle();
+  initMobileMenu();
+  // Обновляет тему на основе системных настроек
+  function updateThemePreference() {
+    const html = document.documentElement;
+    const prefersDarkScheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const themeClass = "dark-theme";
 
-	// Обновляет тему на основе системных настроек
-	function updateThemePreference() {
-		const html = document.documentElement;
-		const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-		const themeClass = 'dark-theme';
+    withoutTransition(() => {
+      if (prefersDarkScheme) {
+        html.classList.add(themeClass);
+        updateButtonState(true);
+      } else {
+        html.classList.remove(themeClass);
+        updateButtonState(false);
+      }
+    });
+  }
 
-		withoutTransition(() => {
-			if (prefersDarkScheme) {
-				html.classList.add(themeClass);
-				updateButtonState(true);
-			} else {
-				html.classList.remove(themeClass);
-				updateButtonState(false);
-			}
-		});
-	}
+  // Настраивает переключение между темами
+  function setupThemeToggle() {
+    const buttons = document.querySelectorAll(".theme-toggle");
 
-	// Настраивает переключение между темами
-	function setupThemeToggle() {
-		const buttons = document.querySelectorAll('.theme-toggle');
+    buttons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
 
-		buttons.forEach(button => {
-			button.addEventListener('click', event => {
-				event.preventDefault();
+        const isDarkMode = button.classList.contains("dark");
 
-				const isDarkMode = button.classList.contains('dark');
+        withoutTransition(() => {
+          document.documentElement.classList.toggle("dark-theme", isDarkMode);
+          updateButtonState(isDarkMode);
+        });
+      });
+    });
+  }
 
-				withoutTransition(() => {
-					document.documentElement.classList.toggle('dark-theme', isDarkMode);
-					updateButtonState(isDarkMode);
-				});
-			});
-		});
-	}
+  // Обновляет состояние кнопок (выделяет активную)
+  function updateButtonState(isDarkMode) {
+    const lightButton = document.querySelector(".theme-toggle.light");
+    const darkButton = document.querySelector(".theme-toggle.dark");
 
-	// Обновляет состояние кнопок (выделяет активную)
-	function updateButtonState(isDarkMode) {
-		const lightButton = document.querySelector('.theme-toggle.light');
-		const darkButton = document.querySelector('.theme-toggle.dark');
+    if (isDarkMode) {
+      lightButton.classList.remove("active");
+      darkButton.classList.add("active");
+    } else {
+      lightButton.classList.add("active");
+      darkButton.classList.remove("active");
+    }
+  }
 
-		if (isDarkMode) {
-			lightButton.classList.remove('active');
-			darkButton.classList.add('active');
-		} else {
-			lightButton.classList.add('active');
-			darkButton.classList.remove('active');
-		}
-	}
+  // Временно отключает анимации
+  function withoutTransition(callback) {
+    const html = document.documentElement;
+    html.style.transition = "none";
+    callback();
+    setTimeout(() => {
+      html.style.transition = "";
+    }, 0);
+  }
 
-	// Временно отключает анимации
-	function withoutTransition(callback) {
-		const html = document.documentElement;
-		html.style.transition = 'none';
-		callback();
-		setTimeout(() => {
-			html.style.transition = '';
-		}, 0);
-	}
+  function initMobileMenu() {
+    const burgerMenu = document.querySelector(".menu-burger");
+    const closeBurgerMenu = document.querySelector(".menu-close");
+    const menu = document.querySelector(".header__menu");
+
+    burgerMenu.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
+
+    closeBurgerMenu.addEventListener("click", () => {
+      menu.classList.remove("active");
+    });
+  }
 });
